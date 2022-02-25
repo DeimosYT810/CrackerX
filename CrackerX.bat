24babc65eccc84a61a3c4e016261787728a2f097
DeimosYT810 <94899953+DeimosYT810@users.noreply.github.com>
2022-02-25 10:35:05 +0100

    Add files via upload

---
+++ CrackerX.bat
@@ -0,0 +1,31 @@
+Title Cracker X
+color 02
+echo ░██╗░░░░░░░██╗██╗███████╗██╗  ░█████╗░██████╗░░█████╗░░█████╗░██╗░░██╗███████╗██████╗░
+echo ░██║░░██╗░░██║██║██╔════╝██║  ██╔══██╗██╔══██╗██╔══██╗██╔══██╗██║░██╔╝██╔════╝██╔══██╗
+echo  ╚██╗████╗██╔╝██║█████╗░░██║  ██║░░╚═╝██████╔╝███████║██║░░╚═╝█████═╝░█████╗░░██████╔╝
+echo   ████╔═████║░██║██╔══╝░░██║  ██║░░██╗██╔══██╗██╔══██║██║░░██╗██╔═██╗░██╔══╝░░██╔══██
+echo    ██╔╝░╚██╔╝░██║██║░░░░░██║  ╚█████╔╝██║░░██║██║░░██║╚█████╔╝██║░╚██╗███████╗██║░░██║
+echo ░░░╚═╝░░░╚═╝░░╚═╝╚═╝░░░░░╚═╝  ░╚════╝░╚═╝░░╚═╝╚═╝░░╚═╝░╚════╝░╚═╝░░╚═╝╚══════╝╚═╝░░╚═╝
+echo YA ESTA ENCENDIDO!
+pause
+echo quieres comenzar?
+pause 
+echo Pulsa enter para seguir y si quieres salir ESC
+pause
+echo Perfecto.
+pause
+echo Encontrando problemas...
+pause
+echo emos encontrado 1 problema!
+pause
+echo tienes de reiniciar el ordenador.
+pause
+echo reiniciar?
+pause
+echo (y/n)
+pause
+echo Reiniciando...
+pause
+echo apaga el ordenador y vuelvelo a encender ya lo hemos solucionado
+pause
+start CrackerX.py
